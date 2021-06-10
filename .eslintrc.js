88b9937e082bc9ce3e1f4f451f8fab5589455699
wanghqr <455626828@qq.com>
2021-06-10 10:09:51 +0800

    update lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-09 16:26:26
- * @LastEditTime: 2021-06-09 16:30:36
+ * @LastEditTime: 2021-06-10 10:08:31
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /vite-vue3-starter/.eslintrc.js
@@ -12,20 +12,14 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    'airbnb-base',
-    'plugin:prettier/recommended'
-  ],
+  extends: ['plugin:vue/essential', 'airbnb-base', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 12,
     parser: '@typescript-eslint/parser',
-    sourceType: 'module',
+    sourceType: 'module'
   },
-  plugins: [
-    'vue',
-    '@typescript-eslint',
-  ],
+  plugins: ['vue', '@typescript-eslint'],
   rules: {
-  },
-};
+    'no-console': 'off' // off-关闭， 1（warn）-警告， 2（error）-错误
+  }
+}
