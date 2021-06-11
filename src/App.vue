0b89de29ad1a8d672a8c3ae9477762662ed49a31
wanghqr <455626828@qq.com>
2021-06-11 15:28:58 +0800

    feat: update eslint rule

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-06-09 10:03:40
- * @LastEditTime: 2021-06-09 14:53:02
+ * @LastEditTime: 2021-06-10 20:56:13
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /vite-vue3-starter/src/App.vue
@@ -14,12 +14,12 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 })
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
 /*
  * @Author: your name
  * @Date: 2021-06-09 10:03:40
- * @LastEditTime: 2021-06-09 16:02:50
+ * @LastEditTime: 2021-06-10 20:57:47
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /vite-vue3-starter/src/main.ts
  */
-import { createApp } from "vue";
-import App from "./App.vue";
-
-import router from "./router/index";
-import store from "./store/index";
-
 import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
 
-createApp(App).use(router).use(store).use(ElementPlus).mount("#app");
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import router from './router/index'
+import store from './store/index'
+
+const app = createApp(App)
+app.use(ElementPlus)
+app.use(router)
+app.use(store)
+app.mount('#app')
